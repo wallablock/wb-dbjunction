20ee6d6811cf4e9b39649b5a95c4f2c4e67d7a1c
alexlera99 <62151164+alexlera99@users.noreply.github.com>
2020-03-30 13:14:30 +0200

    Update  asyncStartSyncer & getLastBlock

--- src/syncer/index.ts
+++ src/syncer/index.ts
@@ -5,15 +5,61 @@ export function startSyncer(config: Config) {
     asyncStartSyncer(config);
 }
 
+function controlDeErrores(body : any, bulkResponse: any) {
+    
+    const erroredDocuments = []
+    // The items array has the same order of the dataset we just indexed.
+    // The presence of the `error` key indicates that the operation
+    // that we did for the document has failed.
+    bulkResponse.items.forEach((action, i) => {
+      const operation = Object.keys(action)[0]
+      if (action[operation].error) {
+        erroredDocuments.push({
+          // If the status is 429 it means that you can retry the document,
+          // otherwise it's very likely a mapping error, and you should
+          // fix the document before to try it again.
+          status: action[operation].status,
+          error: action[operation].error,
+          operation: body[i * 2],
+          document: body[i * 2 + 1]
+        })
+      }
+    })
+    console.error(erroredDocuments)
+}
+
 async function asyncStartSyncer(config: Config) {
     let blockchain = new Blockchain(config.ethereumNode);
     let syncedUntil = await getLastBlock();
     let syncUpdates = await blockchain.resync((syncedUntil != null) ? syncedUntil : undefined);
     // Process syncUpdates
+    const { Client } = require('@elastic/elasticsearch')
+    const client = new Client({ node: 'https://sync:wallablocksync@f90c7dc79c2b425caf77079b50ec5677.eu-central-1.aws.cloud.es.io:9243/' })
+    
+    const accepted = syncUpdates[2].flatMap(doc => [{ index: { _index: 'offers', _id : doc.id } }, doc])
+    const { accepted: bulkResponse1 } = await client.bulk({ refresh: true, accepted })
+    if (bulkResponse1.errors) controlDeErrores(accepted, bulkResponse1)
+
+    const completed = syncUpdates[3].flatMap(doc => [{ delete: { _index: 'offers', _id : doc.id } }])
+    const { completed: bulkResponse2 } = await client.bulk({ refresh: true, completed })
+    if(bulkResponse2.errors) controlDeErrores(completed, bulkResponse2)
+
+    const cancelled = syncUpdates[4].flatMap(doc => [{ delete: { _index: 'offers', _id : doc.id } }])
+    const { cancelled: bulkResponse3 } = await client.bulk({ refresh: true, cancelled })
+    if (bulkResponse3.errors) controlDeErrores(cancelled, bulkResponse3)
+    
     // Set callbacks
 }
 
 async function getLastBlock(): Promise<string | number | null> {
-    throw "Not implemented";
+    const { Client } = require('@elastic/elasticsearch')
+    const client = new Client({ node: 'https://sync:wallablocksync@f90c7dc79c2b425caf77079b50ec5677.eu-central-1.aws.cloud.es.io:9243/' })
+
+    const { body } = await client.get({
+        index: 'block',
+        id: '1'
+    })
+
+    return body.lastblock
 }
 
